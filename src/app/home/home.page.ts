b0ae3dcaf25ecabc737a707081cddfb99e19c061
hernan <hernag_09@hotmail.com>
2019-09-12 10:42:40 -0300

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  items:any
+  constructor() {
+    
+     setTimeout(()=>this.items=[{'name':'hernan'},{'name':'hernan'},{'name':'hernan'},{'name':'hernan'},{'name':'hernan'},{'name':'hernan'}],3000)
 
-  constructor() {}
-
+  }
+  doRefresh(event) {
+    console.log('evento',event);
+    this.items = null
+    setTimeout(() => {
+      console.log('Async operation has ended');
+      this.items=[{'name':'hernan'},{'name':'hernan'},{'name':'hernan'},{'name':'hernan'},{'name':'hernan'},{'name':'hernan'}]
+      
+      event.target.complete();
+    }, 2000);
+  }
+    
 }
